9c260273968eb54f1c286891f479827b11fba4c1
root <lifangyuan2016@gmail.com>
2018-12-06 15:53:58 +0800

    update@v1

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ var http = require('http');
 var fs = require('fs');
 var dataPath = './outfile';
 var spawn = require('child_process').spawn
+var URL = require('url');
+process.title='bpinfo';
 http.createServer((req,res)=>{
-    res.end(fs.readFileSync(dataPath).toString('utf8'));
-}).listen(80)
+    let path = URL.parse(req.url);
+    switch(path.pathname){
+        case '/':
+            res.end(fs.readFileSync(dataPath).toString('utf8'));
+        break;
+        default:
+            res.end('default');
+    }
+}).listen(4000)
 
 setInterval(function(){
     var ls = spawn('node',['./bp_info.js'])
